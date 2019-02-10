8f7f1b124312db4cbe27ff4d24ba12741d8d8a50
Aaron <shishengyi.china@foxmail.com>
2019-02-10 23:26:11 +0800

    修改proto,添加proto -> go的脚本。

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Pontus服务端的存储层。
 
  - 安装`golang`,`dep`,`protoc`等基本开发工具
  - 运行`cd src && dep init`以生成工程的依赖，产生以下文件：`Gopkg.toml`,`Gopkg.lock`,`vendor/`
- - 运行`cd protos && protoc --go_out=. *.proto`以生成proto的golang文件
+ - 运行`cd protos && bash build.sh`以生成proto的golang文件，脚本运行完成后，即可在`src/pb`目录下看到相关文件。
 
 > 执行上述开发过程时，依赖`dep`可执行文件。此文件并不是`golang`开发环境默认就有的。安装过程也很简单，使用`go get -d -u github.com/golang/dep`即可以安装。
 
--- protos/build.sh
+++ protos/build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+if [ ! -f build.sh ]; then
+    echo 'build.sh must be run within its container folder' 1>&2
+    exit 1
+fi
+
 protoc --go_out=plugins=grpc:. common.proto
 protoc --go_out=plugins=grpc:. device.proto
 protoc --go_out=plugins=grpc:. token.proto
 protoc --go_out=plugins=grpc:. user.proto
 
-cp *.pb.go 
+mkdir ../src/pontus_store_pb
+rm ../src/pontus_store_pb/*.pb.go
+
+cp *.pb.go ../src/pontus_store_pb/
+
+rm *.pb.go
